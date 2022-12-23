31804147e63af0098308149553e2b578351e61c0
jackb0back <54455407+jackb0back@users.noreply.github.com>
2022-12-22 19:35:29 -0500

    Update storeTest.js

--- storeTest.js
+++ storeTest.js
@@ -1,2 +1,6 @@
-StoreItems.push("Test");
-StoreItems.push("Test2");
+StoreItems.push({
+  Name: "OC_STORE.TEST",
+  Desc: "a test item",
+  Download: "data:text/html;base64,dGVzdA==",
+  Type: "TXT"
+});
